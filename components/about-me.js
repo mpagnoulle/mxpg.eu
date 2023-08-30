f6381fd8292a71e0ca4e947c2637509b39b8ca21
Maxime Pagnoulle <mpagnoulle@gmail.com>
2023-08-30 20:02:08 +0300

    Update about-me.js

--- components/about-me.js
+++ components/about-me.js
@@ -6,7 +6,7 @@ import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
 
 export default function AboutMe() {
-  const { ref } = useSectionInView("About");
+  const { ref } = useSectionInView("About", 0.33);
 
   return (
     <motion.section
