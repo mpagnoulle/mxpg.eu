bd90cd3bd5307d72bdc24c191ee00f0561bdb179
Maxime Pagnoulle <mpagnoulle@gmail.com>
2023-08-30 18:02:12 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,11 +3,6 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
-  env: {
-    SMTP_USER: process.env.SMTP_USER,
-    SMTP_PWD: process.env.SMTP_PWD,
-    RECEIVER_EMAIL: process.env.RECEIVER_EMAIL,
-  },
 };
 
 module.exports = nextConfig;
