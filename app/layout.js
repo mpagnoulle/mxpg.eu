255aee050e961eada3bb39a0fed5b8824e07bffa
Maxime Pagnoulle <mpagnoulle@gmail.com>
2023-09-06 13:24:10 +0300

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -6,6 +6,7 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] });
 
