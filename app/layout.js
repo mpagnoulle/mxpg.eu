24e3ff7a33e04d4224a37aa69259a91435dd4e6d
Maxime Pagnoulle <mpagnoulle@gmail.com>
2023-09-06 14:09:05 +0300

    Add Analytics

--- app/layout.js
+++ app/layout.js
@@ -1,3 +1,5 @@
+"use client";
+
 import Header from "@/components/header";
 import "./globals.css";
 import { Inter } from "next/font/google";
@@ -6,16 +8,17 @@ import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
 import { Toaster } from "react-hot-toast";
-import Script from "next/script"
+import { Analytics } from '@vercel/analytics/react';
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Maxime Pagnoulle",
-  description: "Maxime Pagnoulle is a Software Engineer with a passion to turn ideas into user-centric applications",
+  description:
+    "Maxime Pagnoulle is a Software Engineer with a passion to turn ideas into user-centric applications",
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
 
   return (
     <html lang="en" className="!scroll-smooth">
@@ -23,7 +26,10 @@ export default function RootLayout({children}) {
         className={`${inter.className} bg-gray-50 text-gray-950 relative pt-28 sm:pt-36 dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}
       >
         <div className="w-full h-full absolute top-0 left-0 -z-[1]">
-          <div className="absolute bg-gradient h-1/2 w-full" id="bgGradient"></div>
+          <div
+            className="absolute bg-gradient h-1/2 w-full"
+            id="bgGradient"
+          ></div>
           <div className="absolute w-full h-1/2 bg-grid" id="bgGrid"></div>
         </div>
 
@@ -37,16 +43,9 @@ export default function RootLayout({children}) {
             <ThemeSwitch />
           </ActiveSectionContextProvider>
         </ThemeContextProvider>
-        <Script id="gtag-init" strategy="afterInteractive">
-          {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', 'G-RCQEM9C8WN');
-        `}
-</Script>
+        <Analytics />
       </body>
     </html>
-  );y
+  );
+  y;
 }
